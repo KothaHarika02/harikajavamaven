e8c4eea455fb8885021a33f323968a7a6881b49a
KothaHarika02 <kothaharika2000@gmail.com>
2020-02-07 21:08:03 -0800

    adding the project

---
+++ src/main/java/com/harika1/Candiesmaven/App.java
@@ -0,0 +1,13 @@
+package com.harika1.Candiesmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/harika1/Candiesmaven/AppTest.java
@@ -0,0 +1,20 @@
+package com.harika1.Candiesmaven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
